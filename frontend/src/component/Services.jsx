a8d0d6f4a21d37d9caae227a9a0599bbe0465193
Pintu <pintu20081999@gmail.com>
2024-11-16 15:16:27 +0530

    ladai

--- frontend/src/component/Services.jsx
+++ frontend/src/component/Services.jsx
@@ -10,8 +10,12 @@ function Services() {
   ];
 
   return (
-    <div>
-      <h2>Our Services</h2>
+    <div className='d-flex flex-column justify-content-center align-itmes-center'>
+      <div className='services d-flex gap-4 justify-content-center'>
+        <img src="/our_service.png" alt="not found" style={{width:'4rem', height:'4rem', backgroundColor:"#f0f2f5"}}  />
+        <h2>Our Services</h2>
+      </div>
+      
       <ul>
         {services.map((service, index) => (
           <li key={index}>
